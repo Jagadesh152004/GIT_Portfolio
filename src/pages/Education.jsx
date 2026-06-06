f25ae4221310501c035f9dbc9f320507edbdc903
Jagadesh152004 <jagadeshwaran152004@gmail.com>
2026-06-06 16:01:42 +0530

    new style in single varaible

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -4,6 +4,12 @@ import {Link} from "react-scroll"
 
 
 const Education = () => {
+
+  const content = 
+  "w-md bg-black border-2 border-cyan-400 rounded-4xl p-8 text-center shadow-[0_0_25px_#22d3ee] transition-all hover:scale-110 duration-600 ease-in-out cursor-pointer hover:-translate-y-2 hover:shadow-[0_0_50px_#22d3ee]";
+
+  const dot = "absolute left-1/2 top-10 w-5 h-5 bg-cyan-400 rounded-full shadow-[0_0_20px_#22d3ee] -translate-x-1/2";
+
   return (
     <Element name="education">
     
@@ -24,29 +30,25 @@ const Education = () => {
     <div className="relative flex justify-start mb-5 p-5">
 
 {/* Item1-Content */}
-      <div className="w-md bg-black border-2 border-cyan-400
-      rounded-4xl p-8 text-center
-      shadow-[0_0_25px_#22d3ee] transition-all hover:scale-110 duration-600 ease-in-out cursor-pointer hover:-translate-y-2
-hover:shadow-[0_0_50px_#22d3ee]">
+      <div className={content}>
 
         <h3 className="text-white font-bold text-xl">
           🎓 B.Tech – AI & DS
         </h3>
+
         <p className="text-amber-300 font-medium">
           Kathir College of Engineering, Coimbatore 2022 – 2026 | CGPA: 7.51
-
         </p>
+
         <p className="text-gray-400 mt-3">
           Pursuing a degree in Artificial Intelligence and Data Science while developing skills in 
           software development, web technologies, and problem-solving.
         </p>
+
       </div>
 
             {/* Dot */}
-      <div className="absolute left-1/2 top-10
-      w-5 h-5 bg-cyan-400 rounded-full
-      shadow-[0_0_20px_#22d3ee]
-      -translate-x-1/2">
+      <div className={dot}>
       </div>
 
       </div>
@@ -54,14 +56,12 @@ hover:shadow-[0_0_50px_#22d3ee]">
       {/* Item-2 */}
       <div className="relative flex justify-end mt-2 mb-5 p-5">
         {/* Item-2 Content */}
-      <div className="w-md bg-black border-2 border-cyan-400
-      rounded-4xl p-8 text-center
-      shadow-[0_0_25px_#22d3ee] transition-all hover:scale-110 duration-600 ease-in-out cursor-pointer hover:-translate-y-2
-hover:shadow-[0_0_50px_#22d3ee]">
+      <div className={content}>
 
         <h3 className="text-white font-bold text-xl">
          🏫 HSC
         </h3>
+
         <p className="text-amber-300 font-medium">
           Adwaith GNS Matric Higher Secondary School 
           2022 | 76%
@@ -71,40 +71,36 @@ hover:shadow-[0_0_50px_#22d3ee]">
           Completed my higher secondary education with a strong
           foundation in science and mathematics.
         </p>
+
       </div>
 
             {/* Dot */}
-      <div className="absolute left-1/2 top-10
-      w-5 h-5 bg-cyan-400 rounded-full
-      shadow-[0_0_20px_#22d3ee]
-      -translate-x-1/2">
+      <div className={dot}>
       </div>
 
       </div>
 
       {/* Item-3 */}
       <div className="relative flex justify-start p-5">
-                {/* Item-3 Content */}
-      <div className="w-md bg-black border-2 border-cyan-400
-      rounded-4xl p-8 text-center
-      shadow-[0_0_25px_#22d3ee] transition-all hover:scale-110 duration-600 ease-in-out cursor-pointer hover:-translate-y-2 hover:shadow-[0_0_50px_#22d3ee]">
+      {/* Item-3 Content */}
+      <div className={content}>
 
         <h3 className="text-white font-bold text-xl">
         📚 SSLC
         </h3>
+
         <p className="text-amber-300 font-medium ">
           Adwaith GNS Matric Higher Secondary School 2020 | 77%
         </p>
+
         <p className="text-gray-400 mt-3">
           Successfully completed secondary education, building strong academic and analytical skills.
         </p>
+
       </div>
 
-            {/* Dot */}
-      <div className="absolute left-1/2 top-10
-      w-5 h-5 bg-cyan-400 rounded-full
-      shadow-[0_0_20px_#22d3ee]
-      -translate-x-1/2 transition-all hover:scale-110 duration-600 ease-in-out cursor-pointer hover:-translate-y-2 hover:shadow-[0_0_50px_#22d3ee]">
+      {/* Dot */}
+      <div className={dot}>
       </div>
 
       </div>
